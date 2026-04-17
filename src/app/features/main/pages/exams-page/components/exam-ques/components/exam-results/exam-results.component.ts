b744aeb685b2d7cb17b0c67d6c1613b9401a8c9a
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-04-17 20:41:28 +0200

    modified:   src/app/features/main/pages/exams-page/components/exam-ques/components/exam-results/exam-results.component.css modified:   src/app/features/main/pages/exams-page/components/exam-ques/components/exam-results/exam-results.component.html modified:   src/app/features/main/pages/exams-page/components/exam-ques/components/exam-results/exam-results.component.ts modified:   src/app/features/main/pages/exams-page/components/exam-ques/exam-ques.component.html modified:   src/app/features/main/pages/exams-page/components/exam-ques/exam-ques.component.ts

--- src/app/features/main/pages/exams-page/components/exam-ques/components/exam-results/exam-results.component.ts
+++ src/app/features/main/pages/exams-page/components/exam-ques/components/exam-results/exam-results.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, Component, inject, PLATFORM_ID } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { FormsModule } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+
 
 @Component({
   selector: 'app-exam-results',
-  imports: [],
+  imports: [ChartModule, RadioButtonModule, FormsModule],
   templateUrl: './exam-results.component.html',
   styleUrl: './exam-results.component.css',
 })
 export class ExamResultsComponent {
+
+  data: any;
+  options: any;
+  platformId = inject(PLATFORM_ID);
+  cd = inject(ChangeDetectorRef);
+  wrongAnswer:string = 'wrong';
+  rightAnswer:string = 'right';
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+
+      this.data = {
+        labels: ['Correct : 0', 'Incorrect : 0'],
+        datasets: [
+          {
+            data: [300, 50],
+            backgroundColor: [documentStyle.getPropertyValue('--p-emerald-500'), documentStyle.getPropertyValue('--p-red-500'), documentStyle.getPropertyValue('--p-gray-500')],
+          }
+        ]
+      };
+
+      this.options = {
+        cutout: '60%',
+        plugins: {
+          legend: {
+            position: 'bottom',
+            align: 'start',
+            labels: {
+              color: textColor,
+              boxWidth: 15,
+              padding: 15,
+              align: 'vertical',
+              font: {
+                size: 14,
+                weight: '500',
+                family: 'monospace'
+              }
+            }
+          }
+        }
+      };
+    }
+    this.cd.markForCheck();
+  }
+
 
 }
--- src/app/features/main/pages/exams-page/components/exam-ques/exam-ques.component.ts
+++ src/app/features/main/pages/exams-page/components/exam-ques/exam-ques.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { QuesHeaderComponent } from "./components/ques-header/ques-header.component";
-import { QuestionComponent } from "./components/question/question.component";
+import { ExamResultsComponent } from "./components/exam-results/exam-results.component";
 
 @Component({
   selector: 'app-exam-ques',
-  imports: [QuesHeaderComponent, QuestionComponent],
+  imports: [QuesHeaderComponent, ExamResultsComponent],
   templateUrl: './exam-ques.component.html',
   styleUrl: './exam-ques.component.css',
 })
 export class ExamQuesComponent {
+
+  
 
 }
