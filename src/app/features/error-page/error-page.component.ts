9f92e3f036a7320bbddb9735dd5b7082e48278c5
Ahmed-207 <eng.ahmed.mosaad.207@gmail.com>
2026-04-22 00:20:30 +0200

    es/error-page/error-page.component.css modified:   src/app/features/error-page/error-page.component.html modified:   src/app/features/error-page/error-page.component.ts

--- src/app/features/error-page/error-page.component.ts
+++ src/app/features/error-page/error-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MainButtonComponent } from "../../shared/components/main-button/main-button.component";
+import { Router } from '@angular/router';
+import { LucideAngularModule, CircleX } from 'lucide-angular';
 
 @Component({
   selector: 'app-error-page',
-  imports: [],
+  imports: [MainButtonComponent, LucideAngularModule],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.css',
 })
 export class ErrorPageComponent {
+
+  private readonly router = inject(Router);
+  readonly circleX = CircleX
+
+  exploreDiplomas(): void {
+    this.router.navigate(['home/diplomas']);
+  }
 
 }
